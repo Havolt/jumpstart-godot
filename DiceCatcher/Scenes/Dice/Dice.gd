a43934458bfcc15983c4f67800a74d62aa878bfa
Mark <markfitz815@gmail.com>
2026-03-17 17:08:21 +0000

    Remove Dice when it hits bottom of page

--- DiceCatcher/Scenes/Dice/Dice.gd
+++ DiceCatcher/Scenes/Dice/Dice.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-const SPEED = 2.0
+const SPEED = 40.0
 const ROTATION_SPEED = 5.0
 var ROTATION_DIRECTION = 1 if randi() % 2 else -1
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +12,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.y += SPEED
-	rotate(ROTATION_SPEED * ROTATION_DIRECTION)
-	print(ROTATION_DIRECTION)
-	pass
+func _physics_process(delta: float) -> void:
+	position.y += SPEED * delta
+	sprite_2d.rotate(ROTATION_SPEED * ROTATION_DIRECTION * delta)
+	check_game_over()
+	
+func check_game_over() -> void:
+	if get_viewport_rect().end.y < position.y:
+		queue_free()
+	
